39a72e34b33c3810b6cb7c77401f067ea2459a4f
Alexandra <alexandra.naranajo@inacapmail.cl>
2026-06-15 18:39:55 -0400

    Etapa 1

---
+++ Album-Mundial/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { stickers } from "./data/stickers";
+
+function App() {
+  console.log(stickers);
+
+  return (
+    <>
+      <h1>Álbum Mundial 2026</h1>
+    </>
+  );
+}
+
+export default App;
