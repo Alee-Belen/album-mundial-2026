6202c41d475b67c1c93b6b9cfdb06c4b7fe20481
Alexandra <alexandra.naranajo@inacapmail.cl>
2026-06-15 19:09:36 -0400

    Etapa 2

--- Album-Mundial/src/App.jsx
+++ Album-Mundial/src/App.jsx
@@ -1,14 +1,27 @@
 import "./App.css";
 import { stickers } from "./data/stickers";
+import StickerCard from "./components/StickerCard";
 
 function App() {
   console.log(stickers);
 
   return (
     <>
-      <h1>Álbum Mundial 2026</h1>
+        <h1>Álbum Mundial 2026</h1>
+
+        {
+            stickers.slice(0, 5).map((sticker) => (
+                <StickerCard
+                    key={sticker.id}
+                    number={sticker.code}
+                    name={sticker.name}
+                    group={sticker.group}
+                    status="falta"
+                />
+            ))
+        }
     </>
-  );
+);
 }
 
 export default App;
---
+++ Album-Mundial/src/components/StickerCard.jsx
@@ -0,0 +1,31 @@
+function StickerCard(props) {
+
+    let color = "";
+
+    if (props.status === "tengo") {
+        color = "lightgreen";
+    }
+
+    if (props.status === "repetida") {
+        color = "khaki";
+    }
+
+    if (props.status === "falta") {
+        color = "lightgray";
+    }
+
+    return (
+        <div
+            style={{backgroundColor: color, padding: "15px", margin: "10px", borderRadius: "10px", color: "black"}}
+            className="sticker-card"
+        >
+            <h3>{props.number}</h3>
+
+            <p>{props.name}</p>
+
+            <p>{props.group}</p>
+        </div>
+    );
+}
+
+export default StickerCard;
